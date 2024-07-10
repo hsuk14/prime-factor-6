850145131868c02ac04ae259c02554663758ce94
h.seok <h.seoK@samsung.com>
2024-07-10 09:45:30 +0900

    [refactoring] #28

--- test_prime_factors.py
+++ test_prime_factors.py
@@ -4,13 +4,16 @@ from prime_factors import PrimeFactors
 
 
 class TestPrimeFactors(TestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.sut = PrimeFactors()
+
     def test_of_1(self):
         expected_result = []
-        sut = PrimeFactors()
-        self.assertEqual(expected_result, sut.of(1))
+        self.assertEqual(expected_result, self.sut.of(1))
 
     def test_of_2(self):
         expected_result = [2]
-        sut = PrimeFactors()
-        self.assertEqual(expected_result, sut.of(2))
+        self.assertEqual(expected_result, self.sut.of(2))
 
